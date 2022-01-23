9737fb03454b7bc642975b82acb804a906bb5e27
josemavit24 <josemavit24@gmail.com>
2022-01-23 12:44:28 -0500

    Added Navbar

---
+++ src/navigation/topnavbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'duo-topnavabar',
+    templateUrl: 'topnavbar.component.html',
+    styleUrls: ['topnavbar.component.css']
+})
+export class TopNavBarComponent{
+
+}
